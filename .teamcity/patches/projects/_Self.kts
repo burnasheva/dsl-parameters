4c559869f01972c509cac4d72d85c307a57bf864
admin <admin@TeamCity>
2019-11-20 14:46:57 +0100

    TeamCity change in 'Project with DSL settings' project: project parameters were changed

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -12,6 +12,9 @@ accordingly, and delete the patch script.
 changeProject(DslContext.projectId) {
     params {
         add {
+            param("parameter name", "value")
+        }
+        add {
             param("§±!@#${'$'}%^&&*", "(&&^%${'$'}##@@")
         }
         add {
